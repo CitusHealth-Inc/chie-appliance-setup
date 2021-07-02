3b46c0ed02963b46123e56fc4eb1ac858f7d6847
Anish Varghese <anish.varghese@citrusinformatics.com>
2021-07-02 19:26:30 +0530

    Update setup.sh

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,18 +1,7 @@
 #!/usr/bin/env bash
-title() {
-    local color='\033[1;37m'
-    local nc='\033[0m'
-    printf "\n${color}$1${nc}\n"
-}
-
 sudo yum groupinstall 'Development Tools'
 title "Install roles from Ansible Galaxy"
 sudo ansible-galaxy install geerlingguy.docker
-sudo ansible-galaxy install bertvv.samba
-
-title "Finish setting up shell"
-zsh -i -c setupsolarized dircolors.256dark
-
 export ASF_HOME=/etc/appliance-setup-framework
 export PLAYBOOKS_SRC=$ASF_HOME/playbooks
 title "Run all numbered playbooks for $(whoami)"
